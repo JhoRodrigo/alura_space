e78a5d2c23c076c2693b084f7dee28ae4d2fbb6d
JhoRodrigo <jrodrigomdc@hotmail.com>
2025-01-01 21:18:24 -0300

    curso 6 aula 3

---
+++ apps/galeria/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from apps.galeria.models import Fotografia
+
+class FotografiaForms(forms.ModelForm):
+    class Meta:
+        model = Fotografia
+        exclude = ['publicada',]
+        labels = {
+            'descricao':'Descrição',
+            'data_fotografia': 'Data de registro',
+            'usuario': 'Usuário',
+        }
+
+        widgets = {
+            'nome': forms.TextInput(attrs={'class':'forms-control'}),
+            'legenda': forms.TextInput(attrs={'class':'forms-control'}),
+            'categoria': forms.Select(attrs={'class':'forms-control'}),
+            'descricao': forms.Textarea(attrs={'class':'forms-control'}),
+            'foto': forms.FileInput(attrs={'class':'forms-control'}),
+            'data_fotografia': forms.DateInput(
+                format='%d/%m/%Y',
+                attrs={
+                    'type':'date',
+                    'class':'forms-control'}),
+            'usuario': forms.Select(attrs={'class':'forms-control'}),
+        }
