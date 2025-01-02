5a44b7eefaf38827ff7c50523de11346592ca930
JhoRodrigo <jrodrigomdc@hotmail.com>
2025-01-01 21:49:07 -0300

    curso 6 aula 4

--- apps/galeria/views.py
+++ apps/galeria/views.py
@@ -28,10 +28,12 @@ def buscar(request):
         if nome_a_buscar:
             fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
 
-    return render(request, 'galeria/buscar.html', {"cards": fotografias})
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
 def nova_imagem(request):
-    
+    if not request.user.is_authenticated:
+        messages.error(request, 'Usuario não logado')
+        return redirect('login')     
      
     form = FotografiaForms
     if request.method == 'POST':
@@ -43,8 +45,28 @@ def nova_imagem(request):
 
     return render(request, 'galeria/nova_imagem.html', {'form': form})
 
-def editar_imagem(request):
-    pass
+def editar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    form = FotografiaForms(instance=fotografia)
 
-def deletar_imagem(request):
-    pass
+    if request.method == 'POST':
+        form = FotografiaForms(request.POST, request.FILES, instance=fotografia)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Fotografia editada com Sucesso!!!')
+            return redirect('index')
+
+    return render(request, 'galeria/editar_html', {'form':form, 'foto_id': foto_id})
+    
+
+def deletar_imagem(request, foto_id):
+    fotografia = Fotografia.objects.get(id=foto_id)
+    fotografia.delete()
+    messages.success(request, 'Deleção feita com Sucesso!!!')
+    return redirect('index')
+
+def filtro(request, categoria):
+    fotografias = Fotografia.objects.order_by('-data_fotografia').filter(publicada=True, categoria=categoria)
+
+    return render(request, 'galeria/index.html', {'cards': fotografias})
+
